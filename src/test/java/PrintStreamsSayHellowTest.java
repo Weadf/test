30994cfa181e0f028661b0b8ae721104fc2236e2
Weadf <sabelfeld.00@gmail.com>
2017-10-09 16:49:54 +0300

    cf

--- src/test/java/PrintStreamsSayHellowTest.java
+++ src/test/java/PrintStreamsSayHellowTest.java
@@ -1,33 +1,28 @@
 import com.task.SayHellow;
-import org.junit.After;
+
 import org.junit.Assert;
-import org.junit.Before;
+
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+
 import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
 import java.util.Locale;
 
-public class PrintStreamsSayHellowTest {
+public class PrintStreamsSayHellowTest extends OutputTest{
 
 
-    protected final ByteArrayOutputStream output = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(output));
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
     @Test
     public void testPrinHellow() throws UnsupportedEncodingException {
         String city ="New York";
         Locale.setDefault(new Locale("en"));
         SayHellow instance = new SayHellow(city);
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY,18);
+        cal.set(Calendar.MINUTE,30);
+        cal.set(Calendar.SECOND,0);
+        instance.dataNewZone = cal.getTime();
         instance.printHello();
-        Assert.assertEquals("Good day, New York!", output.toString());
+        Assert.assertEquals("Good day, New York!", output.toString().trim());
     }
 }
